74bb731eb66605c6612311e914184377334f8e20
Jordan Lambert <jordan.lmb1@gmail.com>
2026-01-14 23:47:54 -0500

    style: remove right margin from telemetry chart

--- components/ui/voltage-chart.tsx
+++ components/ui/voltage-chart.tsx
@@ -16,7 +16,7 @@ export function VoltageChart({ data }: VoltageChartProps) {
     return (
         <div className="h-[300px] w-full">
             <ResponsiveContainer width="100%" height="100%">
-                <LineChart data={data} margin={{ top: 5, right: 5, left: -20, bottom: 0 }}>
+                <LineChart data={data} margin={{ top: 5, right: 0, left: -20, bottom: 0 }}>
                     <CartesianGrid strokeDasharray="3 3" stroke="#334155" />
                     <XAxis
                         dataKey="time"
